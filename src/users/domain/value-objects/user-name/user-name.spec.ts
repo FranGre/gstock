512b4cb3f26898de9f7575c121d280dcb00c178f
FranGre <frangregori61@gmail.com>
2025-08-21 23:15:22 +0200

    test(domain): add test for UserName Value Object

---
+++ src/users/domain/value-objects/user-name/user-name.spec.ts
@@ -0,0 +1,14 @@
+describe('UserName Value Object', () => {
+    it('should create correctly', () => {
+        const userName = new UserName('fran');
+        expect(userName.value()).toBe('fran');
+    });
+
+    it('should throw InvalidLengthUserNameException, not between 2 and 20', () => {
+        expect(() => new UserName('a')).toThrow(new InvalidLengthUserNameException());
+    });
+
+    it('should throw InvalidLengthUserNameException, not between 2 and 20', () => {
+        expect(() => new UserName('123456789012345678901')).toThrow(new InvalidLengthUserNameException());
+    });
+});
